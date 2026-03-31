fe253caedd3523560918fcece077c63e494a59ab
Jorge Eduardo Rivera Araque <jeriveraa23@hotmail.com>
2026-03-30 21:37:41 -0500

    Ajuste pipeline

--- airflow/plugins/elt/models.py
+++ airflow/plugins/elt/models.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Table, Column, Integer, Float, MetaData
-from sqlalchemy.dialects.postgresql import TIMESTAMPTZ
+from sqlalchemy import Table, Column, Integer, Float, MetaData, TIMESTAMP
+
 
 metadata = MetaData()
 
 bronze_weather_current = Table(
     "bronze_weather_current", metadata,
     Column("id",                Integer, primary_key=True, autoincrement=True),
-    Column("fetched_at",        TIMESTAMPTZ, nullable=False),
-    Column("time",              TIMESTAMPTZ, nullable=False),
+    Column("fetched_at",        TIMESTAMP(timezone=True), nullable=False),
+    Column("time",              TIMESTAMP(timezone=True), nullable=False),
     Column("precipitation",     Float),
     Column("rain",              Float),
     Column("showers",           Float),
@@ -19,8 +19,8 @@ bronze_weather_current = Table(
 bronze_weather_hourly = Table(
     "bronze_weather_hourly", metadata,
     Column("id",                Integer, primary_key=True, autoincrement=True),
-    Column("fetched_at",        TIMESTAMPTZ, nullable=False),
-    Column("time",              TIMESTAMPTZ, nullable=False, unique=True),
+    Column("fetched_at",        TIMESTAMP(timezone=True), nullable=False),
+    Column("time",              TIMESTAMP(timezone=True), nullable=False, unique=True),
     Column("precipitation",     Float),
     Column("rain",              Float),
     Column("showers",           Float),
