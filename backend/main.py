05882227b70757677bcc590540b07e1121410750
Jorge Eduardo Rivera Araque <jeriveraa23@hotmail.com>
2026-05-07 11:36:24 -0500

    CORS para back y front

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.risk_controller import router as risk_router
 from app.controllers.precipitation_controller import router as precipitation_router
 
 app = FastAPI(title="Alerto API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 app.include_router(risk_router)
 app.include_router(precipitation_router)
